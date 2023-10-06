369d86a2a57b31691b4391784c092e98e5357015
sere06001 <sere06001@utb.vaxjo.se>
2023-10-07 01:41:56 +0200

    Add project files.

---
+++ Uppgift 3.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Uppgift3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många timmar vill du hyra en bil?");
+            int timmar = int.Parse(Console.ReadLine());
+            int summa = timmar * 80;
+            if (summa >= 950)
+            {
+                Console.WriteLine("Det kommer att kosta dig 950kr");
+            }
+            else
+            {
+                Console.WriteLine("Det kommer att kosta dig " + summa + "kr.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
